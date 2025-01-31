2e95308edc1890df5d1d72d629e25e272e42c9f2
Justin Rodriguez <jjrod297@gmail.com>
2025-01-30 23:52:56 -0800

    Menu loop successful and options 1&2 are working as intended. Saving as a check point to continue option 3 and any remaining functions.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 // Course: COMP-003A 
 // Faculty: Jonathan Cruz 
 // Purpose: Budget management application demonstrating control flow. 
+using System.ComponentModel.Design;
+
 namespace COMP0003A.CodingAssignment3
 {
     internal class Program
@@ -10,21 +12,58 @@ namespace COMP0003A.CodingAssignment3
         {
             Console.WriteLine("Welcome to the Budget Management Tool!");
 
-            // Prompt the user to enter their current monthly income
-            int income;
+
+            // Initialize variables
+            int income = 0;
+            int input;
+            string expenseName1 = "";
+            int expenseAmount1 = 0;
+
+            // Prompt user to enter their income
             Console.Write("\nEnter your monthly income: ");
             income = int.Parse(Console.ReadLine());
 
-            // Display menu and options 
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Add an Expense");
-            Console.WriteLine("2. View Expenses and Budget");
-            Console.WriteLine("3. Remove and Expense");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("Enter your choice: ");
+            // Display menu  
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1, Add an Expense");
+                Console.WriteLine("2. View Expenses and Budget");
+                Console.WriteLine("3. Remove an Expense");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+                input = int.Parse(Console.ReadLine());
 
+                if (input == 1)
+                {
+                    Console.Write("\nEnter the expense name: ");
+                    expenseName1 = Console.ReadLine();
+                    Console.Write("Enter the expense amount: ");
+                    expenseAmount1 = int.Parse(Console.ReadLine());
+                    income -= expenseAmount1; // Update net income 
+                    Console.WriteLine("Expense Added Successfully!");
+                }
+              else if (input == 2)
+                {
+                    Console.WriteLine("\nExpenses");
+                    Console.WriteLine($"- {expenseName1}: ${expenseAmount1}");
+                    Console.WriteLine($"Total Expenses: ${expenseAmount1}");
+                    Console.WriteLine($"Remaining Budget: ${income}");
+                }
+
+
+
+            }
 
             
+            
+            
+
+
+
+
+
+
 
 
         }
